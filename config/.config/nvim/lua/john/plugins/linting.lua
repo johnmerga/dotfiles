e6bee7b5b2ccef09eab3a3619a65ab644a16e3c9
John Merga <johnmerga@yahoo.com>
2024-10-29 11:40:49 +0300

    fix: fix lint config related to js/ts files

--- config/.config/nvim/lua/john/plugins/linting.lua
+++ config/.config/nvim/lua/john/plugins/linting.lua
@@ -17,17 +17,17 @@ return {
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>ll", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-    -- fix current file
-    vim.keymap.set("n", "<leader>lf", "mF:%!eslint_d --stdin --fix-to-stdout<CR>`F", { desc = "Fix current file" })
+    -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    --   group = lint_augroup,
+    --   callback = function()
+    --     lint.try_lint()
+    --   end,
+    -- })
+    --
+    -- vim.keymap.set("n", "<leader>ll", function()
+    --   lint.try_lint()
+    -- end, { desc = "Trigger linting for current file" })
+    -- -- fix current file
+    -- vim.keymap.set("n", "<leader>lf", "mF:%!eslint_d --stdin --fix-to-stdout<CR>`F", { desc = "Fix current file" })
   end,
 }
